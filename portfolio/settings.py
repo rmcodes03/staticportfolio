9ef51bbddd4c0f01c2725eac511e9907dbbb725a
rmcodes03 <mishrarisabh2000@gmail.com>
2023-10-10 16:31:39 +0530

    added Procfile

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'portfolio/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
